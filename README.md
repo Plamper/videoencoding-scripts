bdbdb41994cf411abdc955cd025e674843a5ad86
Felix Plamper <felix.plamper@tuta.io>
2024-03-18 20:03:47 +0100

    Init flake and packages
    Signed-off-by Felix Plamper <felix.plamper@tuta.io>

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # videoediting-scripts
+
+This repo contains my python scripts for reencoding/editing
+videos with the help of the awesome av1an and multiple
+other pieces of software.
+
+# Installation
+
+If you want to use these scripts to you just need to have nix
+setup with flakes and use the command `nix develop` to automatically
+install all the tools needed for the scripts to run. Alternatively
+nix direnv works too.
---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    vs-overlay.url = "github:nix-community/vs-overlay";
+  };
+  outputs = { self, nixpkgs, flake-utils, vs-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import vs-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          vapour = pkgs.vapoursynth.withPlugins [
+            (pkgs.callPackage ./pkgs/vapoursynth-lsmash.nix { })
+            pkgs.ffms
+          ];
+
+          python= pkgs.python3.withPackages
+            (ps: with ps; [
+              multiprocess
+              pymediainfo
+            ]
+            );
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+              opusTools
+              libaom
+              rav1e
+              ffmpeg-full
+              vapour
+              (callPackage ./pkgs/av1an.nix { vapoursynth = vapour; })
+              (callPackage ./pkgs/svt-av1-psy.nix { })
+              mediainfo
+              python
+            ];
+          };
+        }
+      );
+}
